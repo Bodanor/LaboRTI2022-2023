683dbc3537fb33fe9519e816dd71775ba46a6e13
Liwinux <christos.papadopoulos@student.hepl.be>
2023-10-03 01:58:35 +0200

    Added Login functions

--- Server/src/OVESP.c
+++ Server/src/OVESP.c
@@ -11,223 +11,269 @@
  * 
  */
 
-static int OVESP_SEND(char *requete, int server_socket);
-static int OVESP_RECEIVE(char **reply, int server_socket);
-static int OVESP_RETRIEVE_STATUS(char **reply, int server_socket);
+typedef struct ovesp_t {
+    long tokens;
+    char **tokensData;
+} OVESP;
 
-int OVESP_RETRIEVE_STATUS(char **reply, int server_socket)
+
+/**
+ * @brief Retrieve every token in a request separated by a '#' character.
+ * 
+ * @param request The request to retrieve tokens from.
+ * @return OVESP* : A pointer to a struct containing all the tokens as well a the number of tokens.
+ * @return NULL : If a malloc error occured.
+ */
+static OVESP* OVESP_RETRIEVE_TOKENS(char *request);
+
+/**
+ * @brief Send the request to a socket.
+ * 
+ * @param request The request to send.
+ * @param dst_socket The socket to send the request to.
+ * @return 0: If successfull.
+ * @return -1 : If the socket has been disconnected. 
+ * @return -2 : If a bad parameter has been passed to the function.
+ * @return -3 : If the data could no be sent. That doesn't mean that the socket is closed or the connection is broken !
+ */
+static int OVESP_SEND(const char *request, int dst_socket);
+
+/**
+ * @brief Main login logic function for a login request.
+ * 
+ * @param request_tokens Tokens used to perform the connection.
+ * @param client_socket The socket used to send a connection status.
+ * @return 0: If successfull.
+ * @return -1 : If the socket has been disconnected. 
+ * @return -2 : If a bad parameter has been passed to the function.
+ * @return -3 : If the data could no be sent. That doesn't mean that the socket is closed or the connection is broken !
+ */
+static int OVESP_LOGIN_OPERATION(OVESP *request_tokens, int client_socket);
+
+/**
+ * @brief Receive a OVESP request and tokenize it.
+ * 
+ * @param reply_tokens A pointer to pointer to OVESP struct that will hold all the tokens from the request.
+ * @param dst_socket The source socket to receive a OVESP request.
+ * @return 0 : If the request has successfully been sent.
+ * @return -1 : If the socket has been disconnected. 
+ * @return -2 : If the data is corruped or a malloc call failed.
+ * @return -3 : If the data could no be received. That doesn't mean that the socket is closed or the connection is broken !
+ * 
+ */
+static int OVESP_RECEIVE(OVESP **reply_tokens, int src_socket);
+
+/**
+ * @brief Deallocate memory for a pointer to OVESP struct.
+ * 
+ * @param ovesp The pointer to deallocate memory from.
+ */
+static void destroy_OVESP(OVESP *ovesp);
+
+int OVESP_RECEIVE(OVESP **reply_tokens, int src_socket)
 {
-    int error;
-    char *response;
-
-    response = NULL;
-    
-    error = OVESP_RECEIVE(&response, server_socket);
-    if(error <= 0)
-    {
-        return error; /* Deconnexion du serveur*/
-    }
-    
-    strtok(response, "#"); /* Used to get the next parameter in the reponse */
-    *reply = strtok(response, "#");
-    
-    return (strlen(*reply));
-}
-
-int OVESP_SEND(char *requete, int server_socket)
-{
-    int rep;
-    
-    rep = Send_msg(server_socket, (uint8_t*)requete, strlen(requete));
-    
-    return rep;
-}
-
-/* Always free the returned reply */
-int OVESP_RECEIVE(char **reply, int server_socket)
-{
-    int rep;
+    int error_check;
     Message *msg;
 
     msg = NULL;
-    *reply = NULL;
-    
-    rep = Receive_msg(server_socket, &msg);
-    
-    if (rep > 0) {
-        *reply = (char *)malloc(sizeof(char)*msg->data_size);
-        if (reply == NULL) {
-            destroyMessage(msg);
-            return -2;
-        }
 
-        memcpy(*reply, msg->data, msg->data_size);
-    }
+    error_check = Receive_msg(src_socket, &msg);
+    if (error_check < 0)
+        return error_check;
     
-    destroyMessage(msg);
-    return rep;
-        
+    *reply_tokens = OVESP_RETRIEVE_TOKENS((char*)msg->data);
+    /* If an error occured, destroy the message and return */
+    if (*reply_tokens == NULL) {
+        destroyMessage(msg);
+        return -2;
+    }
+    //TODO: destroy message, possible erreur avec OVESP_RETRIEVE_TOKENS qui copie simplement le pointeur et pas la chaine...
+    
+    return 0;
 }
-int OVESP(char *requete, char *reponse, int socket)
+int OVESP_SEND(const char *request, int dst_socket)
 {
-    char *champs1 = strtok(requete, "#");
-    char *champs2;
-    char *champs3;
-    char *champs4;
-    int rep, rep1;
+    int error_check;
 
-    if (strcmp(champs1, "LOGIN") == 0)
-    {
-        champs2 = strtok(requete, "#");
-        champs3 = strtok(requete, "#");
-        champs4 = strtok(requete, "#");
+    error_check = 0;
 
-        if(strcmp(champs4, "O") == 0)
-        {
-            if((rep = client_check_creds(champs2, champs3)) == 0)
-            {
+    error_check = Send_msg(dst_socket, (uint8_t*)request, strlen(request) + 1);
+    if (error_check > 0)
+        return 0; /* Return 0 if successfull */
 
-                rep1 = OVESP_SEND("LOGIN#OK#", socket);
+    return error_check;
 
-            }
-            else if (rep == 1)
-            {
-                rep1 = OVESP_SEND("LOGIN#KO_Baduser#", socket);
-            }
-            else if (rep == 2)
-            {
-                rep1 = OVESP_SEND("LOGIN#KO_Badpassword#", socket);
-            }
-            else 
-            {
-                rep1 = OVESP_SEND("LOGIN#KO_DB_FAIL#", socket);
-            }
-        }
-        else
-        {
-            if((rep = create_new_user(champs2, champs3)) == 0)
-            {
-
-                rep1 = OVESP_SEND("LOGIN#OK#", socket);
-
-            }
-            if (rep == 1)
-            {
-                rep1 = OVESP_SEND("LOGIN#KO_Userexists#", socket);
-            }
-            else if(rep == -1)
-            {
-                rep1 = OVESP_SEND("LOGIN#KO_DB_FAIL#", socket);
-            }
-        }
-
-        return rep1;
-    }
 }
-/**
- * @brief 
- * 
- * @param user 
- * @param password 
- * @param new_user_flag 
- * @param server_socket 
- * @return 0 : Good
- * @return 1 : bad user
- * @return 2 : bad password
- * @return 3 : User already present
- * @return 4 : Server DB fail
- * @return -1 : Server RIP
- * @return -2 : Network error
- * @return -3 : Network error
- */
+void destroy_OVESP(OVESP *ovsp)
+{
+    free(ovsp->tokensData);
+    free(ovsp);
+}
+int OVESP_LOGIN_OPERATION(OVESP *request_tokens, int client_socket)
+{   
+    int error_check;
+    char buffer[200];
+
+    error_check = 0;
+    /* If new user is asked we check in the database if it already exists */
+    if (strcmp(request_tokens->tokensData[3], "1") == 0) {
+        error_check = create_new_user(request_tokens->tokensData[1], request_tokens->tokensData[2]);
+        if (error_check == 1) {
+            strcpy(buffer, LOGIN_FAIL(LOGIN_ALREADY_EXISTS));
+        }
+        else if (error_check == -1) {
+            strcpy(buffer, LOGIN_FAIL(LOGIN_DB_FAIL));
+        }
+        else if (error_check == 0) {
+            strcpy(buffer, LOGIN_OK);
+        }
+    }
+    else if (strcmp(request_tokens->tokensData[3], "0") == 0) {
+        error_check = client_check_creds(request_tokens->tokensData[1], request_tokens->tokensData[2]);
+        if (error_check == 0) {
+            strcpy(buffer, LOGIN_OK);
+        }
+        else if (error_check == 1) {
+            strcpy(buffer, LOGIN_FAIL(LOGIN_BAD_USER));
+        }
+        else if (error_check == 2) {
+            strcpy(buffer, LOGIN_FAIL(LOGIN_BAD_PASSWORD));
+        }
+        else if (error_check == -1) {
+            strcpy(buffer, LOGIN_FAIL(LOGIN_DB_FAIL));
+        }
+    }
+    else {
+        printf("%s\n", request_tokens->tokensData[3]);
+        strcpy(buffer, LOGIN_FAIL(LOGIN_BAD_REQUEST));
+    }
+
+    return OVESP_SEND(buffer, client_socket);
+
+}
+OVESP* OVESP_RETRIEVE_TOKENS(char *request)
+{
+    int tokensCount;
+    char *tokens_ptr;
+    OVESP *tokens;
+
+    tokensCount = 1;
+    tokens_ptr = request;
+
+    /* Count how many tokens are present in the request */
+    while ((tokens_ptr = strchr(tokens_ptr, '#'))!= NULL){
+        tokensCount++;
+        tokens_ptr++;
+    }
+
+    /* Allocate memory for the tokens*/
+    tokens = (OVESP*)malloc(sizeof(OVESP));
+    if (tokens == NULL)
+        return NULL;
+    
+    /* Allocate enough pointers for the tokens */
+    tokens->tokens = tokensCount;
+    tokens->tokensData = (char**)malloc(sizeof(char*)*tokens->tokens);
+    if (tokens->tokensData == NULL) {
+        free(tokens); /* Free previously allocated pointer before returning. */
+        return NULL;
+    }
+    
+    
+    /* Initial string request will be modified !!!! */
+    tokens_ptr = strtok(request, "#");
+    tokensCount = 0;
+    
+    /* While all tokens are not retrieved we loop*/
+    while (tokens_ptr != NULL) {
+        
+        tokens->tokensData[tokensCount++] = tokens_ptr; /* Copy the pointer from strktok to the righ token index */
+        tokens_ptr = strtok(NULL, "#");
+    }
+    
+    return tokens;
+}
+
+
+int OVESP_server(char *request, int client_socket)
+{
+    OVESP* tokens;
+    int error_check;
+
+    error_check = 0;
+
+    tokens = OVESP_RETRIEVE_TOKENS(request);
+    if (tokens == NULL) {
+        return -2; /* Server will catch it, maybe sent an error to the client to try again ?*/
+    }
+    if (strcmp(tokens->tokensData[0], LOGIN_COMMAND) == 0) {
+        error_check = OVESP_LOGIN_OPERATION(tokens, client_socket);
+    }
+
+    destroy_OVESP(tokens);
+
+    return error_check;
+}
+
 int OVESP_Login(const char *user, const char *password, const char new_user_flag, int server_socket)
 {
-    char requete[100];
-    int error;
-    char *response;
+    int error_check;
+    char buffer[200];
+    OVESP *ovesp;
 
-    sprintf(requete, "LOGIN#%s#%s#%c#", user, password, new_user_flag);
-    error = OVESP_SEND(requete, server_socket);
-    if (error < 0)
-        return error;
+    error_check = 0;
+
+    sprintf(buffer, "%s#%s#%s#%c", LOGIN_COMMAND, user, password, new_user_flag);
+
+    error_check = OVESP_SEND(buffer, server_socket);
+    /* if an error occured we return the return statement from the OVESP_SEND function */
+    if (error_check < 0)
+        return error_check;
     
-    error = OVESP_RETRIEVE_STATUS(&response, server_socket);
-    if (error < 0)
-        return error;
+    error_check = OVESP_RECEIVE(&ovesp, server_socket);
+    if (error_check < 0)
+        return error_check;
     
-    if(strcmp(response, LOGIN_OK) == 0)
-        return 0;
-    else if (strcmp(response, LOGIN_BAD_USER) == 0 )
-        return 1;
-    else if (strcmp(response, LOGIN_BAD_PASSWORD) == 0 )
-        return 2;
-    else if (strcmp(response, LOGIN_ALREADY_EXISTS) == 0)
-        return 3;
-    else if (strcmp(response, LOGIN_DB_FAIL) == 0)
-        return 4;
-    /* else jsp*/
+    if (strcmp(ovesp->tokensData[0], LOGIN_COMMAND) == 0) {
+        
+        if (strcmp(ovesp->tokensData[1], SUCCESS) == 0) {
+            destroy_OVESP(ovesp);
+            /* The login attempt has succeeded */
+            return 0;
+        }
+        else if (strcmp(ovesp->tokensData[1], FAIL) == 0) {
+            /* The login attempt has failed. Let's find out why ??? */
+            if (strcmp(ovesp->tokensData[2], LOGIN_BAD_USER) == 0) {
+                destroy_OVESP(ovesp);
+                return 1;
+            }
+            else if (strcmp(ovesp->tokensData[2], LOGIN_BAD_PASSWORD) == 0) {
+                destroy_OVESP(ovesp);
+                return 2;
+            }
+            else if (strcmp(ovesp->tokensData[2], LOGIN_DB_FAIL) == 0) {
+                destroy_OVESP(ovesp);
+                return 3;
+            }
+            else if (strcmp(ovesp->tokensData[2], LOGIN_ALREADY_EXISTS) == 0) {
+                destroy_OVESP(ovesp);
+                return 4;
+            }
+            else {
+                destroy_OVESP(ovesp);
+                return -4; /* Bad reply from the server */
+            }
+        }  
+        else {
+            destroy_OVESP(ovesp);
+            return -4; /* Bad reply from the server */
+        }
+    }
+    else {
+        destroy_OVESP(ovesp);
+        return -4; /* Bad reply from the server */
+    }
     
-}
-int OVESP_Consult(int idArticle, int server_socket)
-{
-    char requete[100];
-    int error_handling;
-
-    sprintf(requete, "CONSULT#%d", idArticle);
-    if ((error_handling = OVESP_SEND(requete, server_socket)) == 0 || error_handling == -1)
-        return error_handling;
-}
-int OVESP_Achat(int idArticle, int quantite, int server_socket)
-{
-    char requete[100];
-    int error_handling;
-
-    sprintf(requete, "ACHAT#%d#%d", idArticle, quantite);
-    if ((error_handling = OVESP_SEND(requete, server_socket)) == 0 || error_handling == -1)
-        return error_handling;
-}
-int OVESP_Caddie(int server_socket)
-{
-    char requete[100];
-    int error_handling;
-
-    sprintf(requete, "CADDIE#");
-    if ((error_handling = OVESP_SEND(requete, server_socket)) == 0 || error_handling == -1)
-        return error_handling;
-}
-int OVESP_Cancel(int idArticle, int server_socket)
-{
-    char requete[100];
-    int error_handling;
-
-    sprintf(requete, "CANCEL#%d", idArticle);
-    if ((error_handling = OVESP_SEND(requete, server_socket)) == 0 || error_handling == -1)
-        return error_handling;
-}
-int OVESP_Cancel_All(int server_socket)
-{
-    char requete[100];
-    int error_handling;
-
-    sprintf(requete, "CANCELALL#");
-    if ((error_handling = OVESP_SEND(requete, server_socket)) == 0 || error_handling == -1)
-        return error_handling;
-}
-int OVESP_Confirmer(int server_socket)
-{
-    char requete[100];
-    int error_handling;
-
-    sprintf(requete, "CONFIRMER#");
-    if ((error_handling = OVESP_SEND(requete, server_socket)) == 0 || error_handling == -1)
-        return error_handling;
-}
-int OVESP_Logout(int server_socket)
-{
-    char requete[100];
-    int error_handling;
-
-    sprintf(requete, "LOGOUT#");
-    if ((error_handling = OVESP_SEND(requete, server_socket)) == 0 || error_handling == -1)
-        return error_handling;
+    destroy_OVESP(ovesp);
+    return 0;
 }
